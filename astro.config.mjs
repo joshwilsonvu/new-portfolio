4fbcd3c7c061b68583f0fd7859802cf4f01fc5f3
Josh Wilson <joshwilsonvu@gmail.com>
2023-04-10 23:14:09 -0400

    Basic styling up.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,9 @@
 import { defineConfig } from 'astro/config';
 
+import solidJs from "@astrojs/solid-js";
+
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [solidJs()],
+  output: 'server'
+});
